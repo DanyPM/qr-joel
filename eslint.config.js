91b9f64916f38135248516765c7d6c5fe33b3041
DanyPM <94061157+DanyPM@users.noreply.github.com>
2025-10-27 17:23:08 +0000

    Render only configured messenger buttons

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import prettierConfig from "eslint-config-prettier";
 
+// eslint-disable-next-line @typescript-eslint/no-deprecated -- config helper keeps shared setup concise
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
